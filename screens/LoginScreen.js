e549b341adce6224f3ea17e59eee0e812da730b4
Grany35 <ogun.ergin35@gmail.com>
2023-06-15 23:06:25 +0300

    Complete LoginScreen UI

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,17 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Dimensions,
+  Image,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import React from "react";
 import { Entypo } from "@expo/vector-icons";
+import colors from "../constants/colors";
+
+const { width } = Dimensions.get("window");
 
 const LoginScreen = () => {
   return (
@@ -8,10 +19,16 @@ const LoginScreen = () => {
       style={{
         flex: 1,
         backgroundColor: "#040306",
-        justifyContent:"flex-end",
+        justifyContent: "flex-end",
       }}
     >
-      <View style={{ alignItems: "center", flexDirection: "column",marginBottom:25 }}>
+      <View
+        style={{
+          alignItems: "center",
+          flexDirection: "column",
+          marginBottom: 35,
+        }}
+      >
         <Entypo
           name="spotify"
           size={50}
@@ -21,7 +38,49 @@ const LoginScreen = () => {
         <Text style={[styles.sloganText, { marginTop: 10 }]}>
           Milyonlarca şarkı.
         </Text>
-        <Text style={styles.sloganText}>Spotify'da ücretsiz.</Text>
+        <Text style={[styles.sloganText, { marginBottom: 40 }]}>
+          Spotify'da ücretsiz.
+        </Text>
+        <Pressable
+          style={[styles.pressable, { backgroundColor: colors.green }]}
+        >
+          <Text style={[styles.pressableText, { color: "black" }]}>
+            Ücretsiz Kaydol
+          </Text>
+        </Pressable>
+        <Pressable style={styles.pressable}>
+          <Image
+            source={require("../assets/icon/google.png")}
+            style={styles.icon}
+          />
+          <Text style={styles.pressableText}>Google ile devam et</Text>
+        </Pressable>
+        <Pressable style={styles.pressable}>
+          <Image
+            source={require("../assets/icon/facebook.png")}
+            style={styles.icon}
+          />
+          <Text style={styles.pressableText}>Facebook ile devam et</Text>
+        </Pressable>
+        <Pressable style={styles.pressable}>
+          <Image
+            source={require("../assets/icon/apple.png")}
+            style={styles.icon}
+          />
+          <Text style={styles.pressableText}>Apple ile devam et</Text>
+        </Pressable>
+        <Pressable>
+          <Text
+            style={{
+              color: "white",
+              marginTop: 25,
+              fontSize: 16,
+              fontWeight: "600",
+            }}
+          >
+            Oturum aç
+          </Text>
+        </Pressable>
       </View>
     </SafeAreaView>
   );
@@ -35,6 +94,26 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontWeight: "bold",
   },
+  icon: {
+    width: 22,
+    height: 22,
+  },
+  pressable: {
+    marginTop: 11,
+    width: width * 0.85,
+    height: 45,
+    paddingHorizontal: 13,
+    alignItems: "center",
+    borderRadius: 20,
+    flexDirection: "row",
+    borderWidth: 0.8,
+    borderColor: "#C0C0C0",
+  },
+  pressableText: {
+    fontWeight: "600",
+    fontSize: 15,
+    color: "white",
+    flex: 1,
+    textAlign: "center",
+  },
 });
-
-// 040306
